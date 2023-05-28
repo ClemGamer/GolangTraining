208563b0501648be656b59b5f15f91b4fe8c16f0
ClemGamer <tsaiyouchen@gmail.com>
2023-05-28 17:51:33 +0800

    [0]feat: struct

---
+++ 18_type/01_struct/01_define/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+type person struct {
+	name string
+	age  int
+	sex  string
+}
+
+func main() {
+	var p person = person{
+		name: "clement",
+		age:  29,
+		sex:  "male",
+	}
+	fmt.Println(p)
+}
