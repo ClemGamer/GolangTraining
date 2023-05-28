f9b5510d84e53d11b0d8ee4ffebbb78f87990a4f
ClemGamer <tsaiyouchen@gmail.com>
2023-05-28 20:35:51 +0800

    [0]feat: json

---
+++ 19_JSON/01_marshal/03_tags/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Person struct {
+	Name        string `json:"-"`
+	Age         int    `json:"change field"`
+	notExported int    // Lower case is not visible to json
+}
+
+func main() {
+	p := Person{"clement", 29, 007}
+	bs, err := json.Marshal(p)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(bs)
+	fmt.Printf("%T\n", bs)
+	fmt.Println(string(bs))
+}
---
+++ 19_JSON/03_encoder/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type Person struct {
+	Name string
+	Age  int
+}
+
+func main() {
+	p := Person{"clement", 29}
+	json.NewEncoder(os.Stdout).Encode(p)
+}
